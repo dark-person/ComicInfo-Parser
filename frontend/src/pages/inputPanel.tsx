25bba3b113668ef692248b8f10ff6f67f0042557
dark-person <66205094+dark-person@users.noreply.github.com>
2023-12-02 21:10:30 +0800

    Create Only Layout for Book Metadata input

--- frontend/src/pages/inputPanel.tsx
+++ frontend/src/pages/inputPanel.tsx
@@ -1,6 +1,45 @@
 // React Component
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
+import { useState } from "react";
+import { Row, Col } from "react-bootstrap";
+
+/** Button Props Interface for InputPanel */
+type InputProps = {
+	// returnFunc: (event: React.MouseEvent) => void;
+};
+
+function FormRow(props: { title: string; inputType?: string; value?: string }) {
+	return (
+		<Form.Group as={Row} className="mb-3">
+			<Form.Label column sm="2">
+				{props.title}
+			</Form.Label>
+			<Col sm="9">
+				<Form.Control type={props.inputType} value={props.value} />
+			</Col>
+		</Form.Group>
+	);
+}
+
+function BookMetadata() {
+	return (
+		<div>
+			<Form>
+				<FormRow title={"Title"} />
+				<FormRow title={"Summary"} />
+				<FormRow title={"Number"} inputType="number" />
+				<FormRow title={"Year"} inputType="number" />
+				<FormRow title={"Month"} inputType="number" />
+				<FormRow title={"Day"} inputType="number" />
+				<FormRow title={"Web"} />
+				<FormRow title={"GTIN"} />
+			</Form>
+		</div>
+	);
+}
 
 export default function InputPanel() {
 	return (
@@ -10,9 +49,10 @@ export default function InputPanel() {
 				defaultActiveKey="Main"
 				id="uncontrolled-tab-example"
 				className="mb-3">
-				<Tab eventKey="Main" title="Main">
-					Tab content for Home
+				<Tab eventKey="Main" title="Book Metadata">
+					<BookMetadata />
 				</Tab>
+
 				<Tab eventKey="Creator" title="Creator">
 					Tab content for Profile
 				</Tab>
