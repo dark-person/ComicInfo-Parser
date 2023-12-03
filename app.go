d740409a0c38eaaaba1bb70967a71b291456e417
dark-person <66205094+dark-person@users.noreply.github.com>
2023-12-03 16:22:06 +0800

    Use Struct as Return value as wails not support multiple value return

--- app.go
+++ app.go
@@ -64,33 +64,54 @@ func (a *App) GetDirectoryWithDefault(defaultDirectory string) string {
 	return directory
 }
 
+type ComicInfoResponse struct {
+	ComicInfo    *comicinfo.ComicInfo `json:"ComicInfo"`
+	ErrorMessage string               `json:"ErrorMessage"`
+}
+
 // Get the comic info by scan the given folder.
 // This function will not create/modify the comicinfo xml.
 //
 // This function will return a comicInfo struct, with error message in string.
-func (a *App) GetComicInfo(folder string) (*comicinfo.ComicInfo, string) {
-
+func (a *App) GetComicInfo(folder string) ComicInfoResponse {
 	absPath := folder
 
+	// Check Absolute path is empty or not
 	if absPath == "" {
-		return nil, "folder cannot be empty"
+		return ComicInfoResponse{
+			ComicInfo:    nil,
+			ErrorMessage: "folder cannot be empty",
+		}
 	}
 
 	// Validate the directory
 	isValid, err := scanner.CheckFolder(absPath, scanner.ScanOpt{SubFolder: scanner.Reject, Image: scanner.Allow})
 	if err != nil {
-		return nil, err.Error()
+		return ComicInfoResponse{
+			ComicInfo:    nil,
+			ErrorMessage: err.Error(),
+		}
 	} else if !isValid {
-		return nil, "folder structure is not correct"
+		return ComicInfoResponse{
+			ComicInfo:    nil,
+			ErrorMessage: "folder structure is not correct",
+		}
 	}
 
 	// Load Abs Path
 	c, err := scanner.ScanBooks(absPath)
 	if err != nil {
-		return nil, err.Error()
+		return ComicInfoResponse{
+			ComicInfo:    nil,
+			ErrorMessage: err.Error(),
+		}
 	}
 
-	return c, ""
+	// Return result
+	return ComicInfoResponse{
+		ComicInfo:    c,
+		ErrorMessage: "",
+	}
 }
 
 // Perform Quick Export Action,
--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -158,3 +158,40 @@ export namespace comicinfo {
 
 }
 
+export namespace main {
+	
+	export class ComicInfoResponse {
+	    ComicInfo?: comicinfo.ComicInfo;
+	    ErrorMessage: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new ComicInfoResponse(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.ComicInfo = this.convertValues(source["ComicInfo"], comicinfo.ComicInfo);
+	        this.ErrorMessage = source["ErrorMessage"];
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+
+}
+
