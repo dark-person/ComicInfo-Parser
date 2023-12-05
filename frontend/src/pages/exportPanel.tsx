ae0b242a08f606eaf334f732c8cfc77cd26a0352
dark-person <66205094+dark-person@users.noreply.github.com>
2023-12-05 20:28:14 +0800

    Implement Folder Select of Export Panel

--- frontend/src/pages/exportPanel.tsx
+++ frontend/src/pages/exportPanel.tsx
@@ -1,3 +1,6 @@
+// React
+import { useState } from "react";
+
 // React Component
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
@@ -5,9 +8,23 @@ import Button from "react-bootstrap/Button";
 import { Row, Col } from "react-bootstrap";
 
 // Wails
-import { GetDirectory } from "../../wailsjs/go/main/App";
+import { GetDirectory, GetDirectoryWithDefault } from "../../wailsjs/go/main/App";
 
 export default function ExportPanel() {
+	// Since this is the final step, could ignore the interaction with App.tsx
+	const [exportDir, setExportDir] = useState("");
+
+	function handleSelect() {
+		if (exportDir != "") {
+			GetDirectoryWithDefault(exportDir).then((input) => {
+				setExportDir(input);
+			});
+		} else {
+			GetDirectory().then((input) => {
+				setExportDir(input);
+			});
+		}
+	}
 	return (
 		<div id="Export-Panel" className="mt-5">
 			<h5 className="mb-4">Export to .cbz</h5>
@@ -18,9 +35,10 @@ export default function ExportPanel() {
 					aria-describedby="btn-select-export-folder"
 					type="text"
 					placeholder="select folder.."
+					value={exportDir}
 					readOnly
 				/>
-				<Button variant="secondary" id="btn-select-export-folder">
+				<Button variant="secondary" id="btn-select-export-folder" onClick={handleSelect}>
 					Select Folder
 				</Button>
 			</InputGroup>
