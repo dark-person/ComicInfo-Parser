a119e062af3e7ccbde376cadf4198e6599e5aa04
dark-person <66205094+dark-person@users.noreply.github.com>
2023-12-06 09:55:38 +0800

    Create New Function of custom output zip filepath

--- internal/archive/archive.go
+++ internal/archive/archive.go
@@ -61,6 +61,61 @@ func CreateZip(folderToAdd string) (dest string, err error) {
 	return filepath.Join(folderToAdd, destFileName+".zip"), nil
 }
 
+// Create ZIP File of inputDir, and output the zip to destDir.
+//
+// This function is a variant of CreateZip(), purpose to provide flexibility.
+func CreateZipTo(inputDir string, destDir string) (dest string, err error) {
+	destFileName := filepath.Base(inputDir)
+
+	// Create ZIP File
+	destFile, err := os.Create(filepath.Join(destDir, destFileName+".zip"))
+	if err != nil {
+		return "", err
+	}
+	defer destFile.Close()
+
+	// Zip Writer
+	destZip := zip.NewWriter(destFile)
+	defer destZip.Close()
+
+	// Load File Entries inside folderToAdd
+	entries, err := os.ReadDir(inputDir)
+	if err != nil {
+		return "", err
+	}
+
+	// Loop File inside folderToAdd
+	for _, entry := range entries {
+		filename := entry.Name()
+
+		// Skip Zip file
+		if filename == destFileName+".zip" {
+			continue
+		}
+
+		// Create File inside zip
+		zipFile, err := destZip.Create(filename)
+		if err != nil {
+			return "", err
+		}
+
+		// Open Actual File
+		file, err := os.Open(filepath.Join(inputDir, entry.Name()))
+		if err != nil {
+			return "", err
+		}
+		defer file.Close()
+
+		// Copy file content
+		_, err = io.Copy(zipFile, file)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return filepath.Join(destDir, destFileName+".zip"), nil
+}
+
 // Rename zip file to cbz file, and wrap it with special folder.
 func RenameZip(absPath string) error {
 	dir := filepath.Dir(absPath)
--- internal/archive/archive_test.go
+++ internal/archive/archive_test.go
@@ -87,3 +87,65 @@ func TestCreateZip(t *testing.T) {
 		t.Error("Content 4 missing in zip")
 	}
 }
+
+// Test Create Zip to destination from input folder.
+func TestCreateZipTo(t *testing.T) {
+	// Create a temp directory
+	tempDir := t.TempDir()
+
+	// Separate two folder
+	inputDir := filepath.Join(tempDir, "input")
+	os.MkdirAll(inputDir, 0755)
+
+	outputDir := filepath.Join(tempDir, "output")
+	os.MkdirAll(outputDir, 0755)
+
+	// Create a set of file
+	file1, _ := os.Create(filepath.Join(inputDir, "image1.jpg"))
+	file2, _ := os.Create(filepath.Join(inputDir, "image2.jpg"))
+	file3, _ := os.Create(filepath.Join(inputDir, "image3.jpg"))
+	file4, _ := os.Create(filepath.Join(inputDir, "test.xml"))
+	defer file1.Close()
+	defer file2.Close()
+	defer file3.Close()
+	defer file4.Close()
+
+	// Start Testing Functions
+	dest, err := CreateZipTo(inputDir, outputDir)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Check Dest Filename
+	destFileName := filepath.Base(inputDir)
+	if dest != filepath.Join(outputDir, destFileName+".zip") {
+		t.Errorf("Error Destination file: %v", dest)
+	}
+
+	// Check Zip Content
+	reader, err := zip.OpenReader(dest)
+	if err != nil {
+		t.Error(err)
+	}
+	defer reader.Close()
+
+	list := make(map[string]int, 0)
+	for _, f := range reader.File {
+		list[f.Name] = 1
+	}
+
+	_, exist1 := list["test.xml"]
+	_, exist2 := list["image1.jpg"]
+	_, exist3 := list["image2.jpg"]
+	_, exist4 := list["image3.jpg"]
+
+	if !exist1 {
+		t.Error("Content 1 missing in zip")
+	} else if !exist2 {
+		t.Error("Content 2 missing in zip")
+	} else if !exist3 {
+		t.Error("Content 3 missing in zip")
+	} else if !exist4 {
+		t.Error("Content 4 missing in zip")
+	}
+}
