af586aef0e746bb593bf14e1c1d3b77cfb8572b5
dark-person <66205094+dark-person@users.noreply.github.com>
2023-12-27 20:19:03 +0800

    Prepare testing environment for app.go
    Includes:
    - git ignore
    -  test go file & its structure

---
+++ app_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+// Please note that, the getDirectory method will not be tested
+// due to it will call a file selector, instead of input & output that can be control.
+
+func TestGetComicInfo(t *testing.T) {
+	// TODO: Implementation
+}
+
+func TestQuickExportKomga(t *testing.T) {
+	// TODO: Implementation
+}
+
+func TestExportXml(t *testing.T) {
+	// TODO: Implementation
+}
+
+func TestExportCbz_NoWrap(t *testing.T) {
+	// TODO: Implementation
+}
+
+func TestExportCbz_Wrap(t *testing.T) {
+	// TODO: Implementation
+}
