bf1a86361ed03eb5942487fec796e5b0a0e807ad
dark-person <66205094+dark-person@users.noreply.github.com>
2024-02-15 15:53:25 +0800

    Fix Tag Item still appear when no tags

--- frontend/src/components/Tags.tsx
+++ frontend/src/components/Tags.tsx
@@ -8,13 +8,26 @@ type TagsAreaProps = {
 export function TagsArea({ rawTags }: TagsAreaProps) {
 	/**
 	 * Parse the raw string that contains tags into arrays.
+	 * <p>
+	 * Please note that, split may able to return `[""]` strings array,
+	 * which may result generated a empty tag item.
+	 * Therefore, this function will turn this case in to empty array instead.
+	 *
 	 * @param raw raw string of tags, e.g `"tag1, tag2"`
 	 * @returns array of tags, e.g. `["tag1", "tag2"]`
 	 */
 	function getTagsList(raw: string | undefined): string[] {
 		if (raw === undefined) return [];
 
-		return raw.split(",");
+		let temp = raw.split(",");
+
+		// Prevent Empty Tag list
+		if (temp.length === 1 && temp[0].length == 0) {
+			console.log("empty tag list");
+			return [];
+		}
+
+		return temp;
 	}
 
 	return (
