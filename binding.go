14063f5869b39c8886b021225408e4e9e3067289
dark-person <66205094+dark-person@users.noreply.github.com>
2024-02-26 22:36:01 +0800

    Use `wails` binding for `AgeRating` field

--- binding.go
+++ binding.go
@@ -12,3 +12,25 @@ var AllMangaValue = []struct {
 	{comicinfo.Manga_Yes, "Yes"},
 	{comicinfo.Manga_YesAndRightToLeft, "YesAndRightToLeft"},
 }
+
+// The value of `AgeRating` const to use in wails binding.
+var AllAgeRatingValue = []struct {
+	Value  comicinfo.AgeRating
+	TSName string
+}{
+	{comicinfo.AgeRating_Unknown, "Unknown"},
+	{comicinfo.AgeRating_AdultsOnly18, "AdultsOnly18"},
+	{comicinfo.AgeRating_EarlyChildhood, "EarlyChildhood"},
+	{comicinfo.AgeRating_Everyone, "Everyone"},
+	{comicinfo.AgeRating_Everyone10Plus, "Everyone10Plus"},
+	{comicinfo.AgeRating_G, "G"},
+	{comicinfo.AgeRating_KidsToAdults, "KidsToAdults"},
+	{comicinfo.AgeRating_M, "M"},
+	{comicinfo.AgeRating_MA15Plus, "MA15Plus"},
+	{comicinfo.AgeRating_Mature17Plus, "Mature17Plus"},
+	{comicinfo.AgeRating_PG, "PG"},
+	{comicinfo.AgeRating_R18Plus, "R18Plus"},
+	{comicinfo.AgeRating_RatingPending, "RatingPending"},
+	{comicinfo.AgeRating_Teen, "Teen"},
+	{comicinfo.AgeRating_X18Plus, "X18Plus"},
+}
