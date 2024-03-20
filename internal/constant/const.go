63ad62aeaff00ea652769ecdf2ff24273f645185
dark-person <66205094+dark-person@users.noreply.github.com>
2024-03-20 20:09:20 +0800

    `constant`: Add package to store common value

---
+++ internal/constant/const.go
@@ -0,0 +1,14 @@
+// Package for storing constants for whole project.
+//
+// Constants are include:
+//   - default file location of database file
+package constant
+
+// Root Directory to storing all config files.
+const RootDir = "comicInfo-parser"
+
+// Database filename.
+const DatabaseFile = "storage.db"
+
+// Database type.
+const DatabaseType = "sqlite3"
