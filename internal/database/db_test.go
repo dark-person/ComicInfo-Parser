6d3493b4082cdaff714937e566fcbf4e0d6f4690
dark-person <66205094+dark-person@users.noreply.github.com>
2024-05-05 23:50:53 +0800

    `database`: add func to get `user_version` of sqlite db
    This commit also contains utils for testing to set user_version

--- internal/database/db_test.go
+++ internal/database/db_test.go
@@ -6,6 +6,8 @@ package database
 // It will not include any actual test of any function.
 
 import (
+	"database/sql"
+	"fmt"
 	"os"
 
 	"github.com/sirupsen/logrus"
@@ -16,4 +18,16 @@ func prepareTest() {
 	// Set Log Level & Output
 	logrus.SetOutput(os.Stdout)
 	logrus.SetLevel(logrus.TraceLevel)
+
+	// Prepare Testing Directory
+	os.MkdirAll("testing", 0755)
+}
+
+// Set user_version of a existing database.
+//
+// This function SHOULD be used in testing ONLY.
+func testOnly_setVersion(db *sql.DB, version int) error {
+	query := fmt.Sprintf("PRAGMA user_version = %d", version)
+	_, err := db.Exec(query)
+	return err
 }
