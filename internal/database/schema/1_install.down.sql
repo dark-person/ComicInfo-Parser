2feea00bd80b15a754108b3aba28f4b1fdad6c8f
dark-person <66205094+dark-person@users.noreply.github.com>
2024-05-08 21:55:47 +0800

    `database`: 1st version of schema for migrate

---
+++ internal/database/schema/1_install.down.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS list_prefix;
+
+DROP TABLE IF EXISTS list_suffix;
+
+DROP TABLE IF EXISTS tag_alias;
+
+DROP TABLE IF EXISTS tags;
+
+DROP VIEW IF EXISTS view_tags;
+
+DROP VIEW IF EXISTS view_alias;
+
+PRAGMA user_version = 0;
