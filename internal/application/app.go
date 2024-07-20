9438110dab6a58395edd4e377404d15d64a1ac14
dark-person <66205094+dark-person@users.noreply.github.com>
2024-07-21 00:30:06 +0800

    Separate history related code to file

--- internal/application/app.go
+++ internal/application/app.go
@@ -332,34 +332,3 @@ func (a *App) ExportCbz(inputDir string, exportDir string, c *comicinfo.ComicInf
 	}
 	return ""
 }
-
-// -----------------------------------------------
-
-// Struct that designed for
-// send last input record from history module to frontend.
-type HistoryResp struct {
-	Inputs   []string `json:"Inputs"`
-	ErrorMsg string   `json:"ErrorMsg"`
-}
-
-// Get all user inputted genre from database.
-func (a *App) GetAllGenreInput() HistoryResp {
-	list, err := history.GetGenreList(a.DB)
-
-	if err != nil {
-		return HistoryResp{nil, err.Error()}
-	}
-
-	return HistoryResp{list, ""}
-}
-
-// Get all user inputted publisher from database.
-func (a *App) GetAllPublisherInput() HistoryResp {
-	list, err := history.GetPublisherList(a.DB)
-
-	if err != nil {
-		return HistoryResp{nil, err.Error()}
-	}
-
-	return HistoryResp{list, ""}
-}
