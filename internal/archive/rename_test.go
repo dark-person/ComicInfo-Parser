a3ea4fc26fb098405233e06c17fd93d2613be62e
dark-person <66205094+dark-person@users.noreply.github.com>
2024-09-28 23:12:15 +0800

    Improve test by naming & const

--- internal/archive/rename_test.go
+++ internal/archive/rename_test.go
@@ -6,26 +6,34 @@ import (
 	"testing"
 )
 
+const (
+	_testZip = "hello.zip"
+	_testCbz = "hello.cbz"
+)
+
 // Test Rename Zip archive to .cbz archive, with wrap option is enabled.
-func TestRenameZip_Wrap(t *testing.T) {
+func TestRenameZipWrap(t *testing.T) {
 	// Create a temp directory
 	tempDir := t.TempDir()
 
 	// Create a folder inside temp directory
 	os.MkdirAll(filepath.Join(tempDir, "tmp"), 0755)
 
+	// Prepare zip path
+	zipPath := filepath.Join(tempDir, "tmp", _testZip)
+
 	// Create a zip file
-	file1, _ := os.Create(filepath.Join(tempDir, "tmp", "hello.zip"))
+	file1, _ := os.Create(zipPath)
 	file1.Close()
 
 	// Test Function
-	err := RenameZip(filepath.Join(tempDir, "tmp", "hello.zip"), true)
+	err := RenameZip(zipPath, true)
 	if err != nil {
 		t.Error(err)
 	}
 
 	// Result Verify
-	dest, openErr := os.Open(filepath.Join(tempDir, "tmp", "hello", "hello.cbz"))
+	dest, openErr := os.Open(filepath.Join(tempDir, "tmp", "hello", _testCbz))
 	if openErr != nil {
 		t.Error(openErr)
 	}
@@ -33,25 +41,28 @@ func TestRenameZip_Wrap(t *testing.T) {
 }
 
 // Test Rename Zip archive to .cbz archive, with wrap options is disabled
-func TestRenameZip_NoWrap(t *testing.T) {
+func TestRenameZipNoWrap(t *testing.T) {
 	// Create a temp directory
 	tempDir := t.TempDir()
 
 	// Create a folder inside temp directory
 	os.MkdirAll(filepath.Join(tempDir, "tmp"), 0755)
 
+	// Prepare zip path
+	zipPath := filepath.Join(tempDir, "tmp", _testZip)
+
 	// Create a zip file
-	file1, _ := os.Create(filepath.Join(tempDir, "tmp", "hello.zip"))
+	file1, _ := os.Create(zipPath)
 	file1.Close()
 
 	// Test Function
-	err := RenameZip(filepath.Join(tempDir, "tmp", "hello.zip"), false)
+	err := RenameZip(zipPath, false)
 	if err != nil {
 		t.Error(err)
 	}
 
 	// Result Verify
-	dest, openErr := os.Open(filepath.Join(tempDir, "tmp", "hello.cbz"))
+	dest, openErr := os.Open(filepath.Join(tempDir, "tmp", _testCbz))
 	if openErr != nil {
 		t.Error(openErr)
 	}
