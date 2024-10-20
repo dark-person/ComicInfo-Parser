3154e0049999910b4a726bc9b049c9e0887073cf
dark-person <66205094+dark-person@users.noreply.github.com>
2024-10-21 00:46:25 +0800

    Update application and main pkg

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"embed"
 	"gui-comicinfo/internal/application"
+	"gui-comicinfo/internal/assets"
+	"gui-comicinfo/internal/constant"
+	"os"
+	"path/filepath"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -10,19 +14,31 @@ import (
 )
 
 //go:embed all:frontend/dist
-var assets embed.FS
+var assetsFs embed.FS
 
 func main() {
+	// Get Home Directory
+	home, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+
+	// Get database path
+	path := filepath.Join(home, constant.RootDir, constant.DatabaseFile)
+
+	// Prepare lazydb
+	l := assets.DefaultDb(path)
+
 	// Create an instance of the app structure
-	app := application.NewApp()
+	app := application.NewApp(l)
 
 	// Create application with options
-	err := wails.Run(&options.App{
+	err = wails.Run(&options.App{
 		Title:  "Gui-comicInfo-Parser",
 		Width:  1200,
 		Height: 810,
 		AssetServer: &assetserver.Options{
-			Assets: assets,
+			Assets: assetsFs,
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.Startup,
