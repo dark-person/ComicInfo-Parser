756f9c02bdd450c58368b18ecee7d491832208b1
dark-person <66205094+dark-person@users.noreply.github.com>
2024-10-26 22:59:31 +0800

    Update schema for normalized db

---
+++ internal/assets/schema/2_normalize.down.sql
@@ -0,0 +1,30 @@
+-- Temp table with no foreign keys
+CREATE TABLE "temp_list_inputted" (
+    "category" TEXT NOT NULL,
+    "input" TEXT NOT NULL,
+    PRIMARY KEY("category", "input")
+);
+
+-- Copy existing table contents with category value mapped
+INSERT INTO
+    "temp_list_inputted" ("category", "input")
+SELECT
+    category_name AS category,
+    input
+FROM
+    list_inputted
+    LEFT JOIN category
+where
+    category.category_id = list_inputted.category;
+
+-- Drop table not supported
+DROP TABLE IF EXISTS "list_inputted";
+
+DROP TABLE IF EXISTS "category";
+
+-- Change temp table name
+ALTER TABLE
+    "temp_list_inputted" RENAME TO "list_inputted";
+
+-- SQLite version
+PRAGMA user_version = 1;
