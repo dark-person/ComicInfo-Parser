622c04b557aeaabc3ccbb5b9d3d684e4fee4e5ff
dark-person <66205094+dark-person@users.noreply.github.com>
2024-12-01 15:13:13 +0800

    Add tests for config module

---
+++ internal/config/load_test.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadYaml(t *testing.T) {
+	type testCase struct {
+		path    string
+		want    *ProgramConfig
+		wantErr bool
+	}
+
+	exPath, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	t.Log(exPath)
+
+	tests := []testCase{
+		{"mock/case-normal.yaml", &ProgramConfig{DefaultExport: filepath.Join(exPath, "./my-export")}, false},
+		{"mock/case-typo1.yaml", &ProgramConfig{DefaultExport: ""}, false},
+		{"mock/case-typo2.yaml", &ProgramConfig{DefaultExport: ""}, false},
+		{"mock/not-exist.yaml", nil, true},
+	}
+
+	for idx, tt := range tests {
+		// Load YAML file and check result
+		c, err := LoadYaml(tt.path)
+
+		if tt.wantErr {
+			assert.NotNilf(t, err, "Error should be returned in case %d, but return nil", idx)
+
+			// Ensure default is returned
+			assert.EqualValuesf(t, Default(), c, "Incorrect values in case %d, should be default config", idx)
+
+		} else {
+			assert.EqualValuesf(t, tt.want, c, "Incorrect values in case %d", idx)
+			assert.Nilf(t, err, "Unexpected error in case %d", idx)
+		}
+	}
+}
