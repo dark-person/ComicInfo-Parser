2d862a86140b61a1072b407c80d6d10e84b1edfe
dark-person <66205094+dark-person@users.noreply.github.com>
2024-12-19 23:54:44 +0800

    Extend color radion

--- frontend/src/components/ColoredRadio.tsx
+++ frontend/src/components/ColoredRadio.tsx
@@ -2,13 +2,15 @@ import { ChangeEvent } from "react";
 import { Form } from "react-bootstrap";
 import "./ColoredRadio.css";
 
+type supportedColors = "dark-red" | "dark-orange" | "dark-yellow" | "dark-green" | "dark-blue";
+
 type ColoredRadioProps = {
 	/** HTML element ID. */
 	id?: string;
 	/** Radio button group usage. */
 	name?: string;
 	/** Color to be used for radio.*/
-	color?: "green" | "saddlebrown";
+	color?: supportedColors;
 	/** String to display as label. */
 	label?: string;
 	/** Additional HTML class to append. */
@@ -27,7 +29,7 @@ export default function ColoredRadio(props: Readonly<ColoredRadioProps>) {
 			label={props.label}
 			name={props.name}
 			id={props.id}
-			className={`text-start mt-1 mb-2 cursor-pointer ${props.color} ${props.className}`}
+			className={`text-start mt-1 mb-2 cursor-pointer hide-focus ${props.color} ${props.className}`}
 			checked={props.checked}
 			onChange={props.onChange}
 		/>
