4e687aa7598543407742428be67c97b62a45a933
dark-person <66205094+dark-person@users.noreply.github.com>
2025-01-26 21:42:58 +0800

    Fix folder that only contain `.webp` cannot generate comicinfo (#129)
    * Tidy check image extension function
    * Allow `.webp` file to process

--- internal/scanner/scanner_test.go
+++ internal/scanner/scanner_test.go
@@ -227,3 +227,26 @@ func TestCheckFolder(t *testing.T) {
 	}
 
 }
+
+func TestIsSupportedImg(t *testing.T) {
+	type testCase struct {
+		filename string
+		want     bool
+	}
+
+	tests := []testCase{
+		{"foo/image1.jpg", true},
+		{"foo/image2.png", true},
+		{"foo/image3.jpeg", true},
+		{"foo/image4.webp", true},
+
+		// Not Supported
+		{"foo/abc/", false},
+		{"foo/abc.txt", false},
+		{"foo/abc.docx", false},
+	}
+
+	for _, tt := range tests {
+		assert.Equal(t, tt.want, isSupportedImg(tt.filename))
+	}
+}
