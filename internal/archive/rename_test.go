3e7032d3c2c3c6ad5f606b1a67b320eed9aca169
dark-person <66205094+dark-person@users.noreply.github.com>
2025-03-16 20:15:57 +0800

    Fix unexpected error when custom wrap folder has trailing space

--- internal/archive/rename_test.go
+++ internal/archive/rename_test.go
@@ -30,6 +30,10 @@ func TestRenameZip(t *testing.T) {
 		{"case1.zip", NoWrap(), "case1.cbz"},
 		{"case2.zip", UseDefaultWrap(), "case2/case2.cbz"},
 		{"case3.zip", UseCustomWrap("def"), "def/case3.cbz"},
+
+		// Check custom wrap with space
+		{"case4.zip", UseCustomWrap("ghi "), "ghi/case4.cbz"},
+		{"case5.zip", UseCustomWrap(" jkl"), "jkl/case5.cbz"},
 	}
 
 	for _, tt := range tests {
