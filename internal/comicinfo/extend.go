97defe299ade282401bff9fef8b9928265ec6cd6
dark-person <66205094+dark-person@users.noreply.github.com>
2025-04-12 21:02:15 +0800

    Add comicinfo func for genre, publisher & translator

--- internal/comicinfo/extend.go
+++ internal/comicinfo/extend.go
@@ -5,3 +5,15 @@ package comicinfo
 func (c *ComicInfo) AddTags(tags ...string) {
 	c.Tags = AddValue(c.Tags, tags...)
 }
+
+func (c *ComicInfo) AddGenre(genre ...string) {
+	c.Genre = AddValue(c.Genre, genre...)
+}
+
+func (c *ComicInfo) AddPublisher(publisher ...string) {
+	c.Publisher = AddValue(c.Publisher, publisher...)
+}
+
+func (c *ComicInfo) AddTranslator(translator ...string) {
+	c.Translator = AddValue(c.Translator, translator...)
+}
