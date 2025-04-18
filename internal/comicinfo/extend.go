d14e7f2e5c110be8f19c81b5eee5ef1edbce79d1
dark-person <66205094+dark-person@users.noreply.github.com>
2025-04-18 18:00:21 +0800

    Add function comments

--- internal/comicinfo/extend.go
+++ internal/comicinfo/extend.go
@@ -6,14 +6,20 @@ func (c *ComicInfo) AddTags(tags ...string) {
 	c.Tags = AddValue(c.Tags, tags...)
 }
 
+// Add genres to the comic info container.
+// This function will handle the comma separation automatically.
 func (c *ComicInfo) AddGenre(genre ...string) {
 	c.Genre = AddValue(c.Genre, genre...)
 }
 
+// Add publishers to the comic info container.
+// This function will handle the comma separation automatically.
 func (c *ComicInfo) AddPublisher(publisher ...string) {
 	c.Publisher = AddValue(c.Publisher, publisher...)
 }
 
+// Add translators to the comic info container.
+// This function will handle the comma separation automatically.
 func (c *ComicInfo) AddTranslator(translator ...string) {
 	c.Translator = AddValue(c.Translator, translator...)
 }
