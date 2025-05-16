f66c428685d57874b18c2efabed2c546a0d6e5d5
dark-person <66205094+dark-person@users.noreply.github.com>
2025-05-16 19:30:09 +0800

    Update schema version of assets pkg

--- internal/assets/embed.go
+++ internal/assets/embed.go
@@ -19,7 +19,7 @@ var schema embed.FS
 // Supported database schema version.
 //
 // Developer should change this value when any schema update performed.
-const supportedSchemaVersion = 2
+const supportedSchemaVersion = 3
 
 // Get embedded schema from filesystem.
 // Used for database schema migrations.
