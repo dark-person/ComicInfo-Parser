fa0b3c22b85ef4b3352d690ff532e33b7a23aae6
dark-person <66205094+dark-person@users.noreply.github.com>
2025-07-13 21:54:13 +0800

    Create tab for autofill words

---
+++ frontend/src/views/InputHistoryView.tsx
@@ -0,0 +1,7 @@
+export default function AutofillView() {
+    return (
+        <div id="Autofill-Panel" className="mt-2 mx-5">
+            <h5 className="mb-4">Autofill Setting</h5>
+        </div>
+    );
+}
